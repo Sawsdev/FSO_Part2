51700cf37b4ab35a782f1ef9370e542f62b1b3d3
Sergio Salazar Williams <sawsdevx8@gmail.com>
2024-02-19 15:15:01 -0500

    Add the final step of part 2

--- countries/src/components/Country.jsx
+++ countries/src/components/Country.jsx
@@ -1,9 +1,20 @@
-
+import { useState, useEffect } from "react"
+import countryService from '../services/countries.js'
 
 const Country = ({country}) => {
-    if(!country) {
-        return null
+
+    const [currentWeather, setCurrentWeather] = useState({})
+    const getCountryWeater = () => {
+        countryService
+        . getCountryCapitalWeather(country.capitalInfo.latlng[0], country.capitalInfo.latlng[1])
+        .then(weather => {
+            console.log("current weather", weather);
+            setCurrentWeather(weather)
+        })
     }
+
+    useEffect(getCountryWeater,[])
+
     const {name, area, capital, flags} = country
     const languages = Object.values(country.languages)
     return (
--- countries/src/services/countries.js
+++ countries/src/services/countries.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api'
+const weatherBaseUrl = 'https://api.openweathermap.org/data/2.5/forecast'
+const api_key = '9f37dcda455b1517af0f0f7986d84a2e'
+
 
 
 const getAllCountries = () => {
@@ -15,10 +18,21 @@ const filterCountries = (name, countries) => {
             .filter( country => country.name.common.toLowerCase().includes(name))
 }
 
+const getCountryCapitalWeather = (lat, lon) => {
+    console.log(api_key);
+    const url = `${weatherBaseUrl}?lat=${lat}&lon=${lon}&appid=${api_key}`
+    return axios
+        .get(url)
+        .then( response => {
+            console.log("received weather", response.data);
+            return response.data
+        })
+}
 
 
 
 export default {
     getAllCountries,
-    filterCountries
+    filterCountries,
+    getCountryCapitalWeather
 }
