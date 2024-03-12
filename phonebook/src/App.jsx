b33454e3f391f610418e18de0fc381a0baa875f7
Sergio Salazar Williams <sawsdevx8@gmail.com>
2024-03-12 06:32:32 -0500

    Add phonebook database step 7

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -91,6 +91,13 @@ const App = () => {
           },3000)
           
         })
+        .catch(error => {
+          console.log(error.response.data.error);
+          setNotification({
+            message: error.response.data.error,
+            type: 'error'
+          })
+        })
       
       
     }
